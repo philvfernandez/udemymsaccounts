fd5bdcd4b0f4d3bf9bbb623e3e8d6958ee4878a6
Phil Fernandez <philvfernandez@gmail.com>
2023-02-23 17:08:09 -0700

    Initial project add and commit.

---
+++ src/test/java/com/philf/acconts/AccontsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.philf.acconts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccontsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
