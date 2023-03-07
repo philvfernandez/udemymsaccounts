38e5d4b83f7579616f7a843107acbe1ea8f95e8c
Phil Fernandez <philvfernandez@gmail.com>
2023-03-06 17:47:51 -0700

    Sections 46 - Updated to read properties from Configuration Server.

--- src/main/java/com/philf/accounts/controller/AccountsController.java
+++ src/main/java/com/philf/accounts/controller/AccountsController.java
@@ -1,18 +1,31 @@
 package com.philf.accounts.controller;
 
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+
+import com.philf.accounts.config.AccountsServiceConfig;
 import com.philf.accounts.model.Accounts;
 import com.philf.accounts.model.Customer;
+import com.philf.accounts.model.Properties;
 import com.philf.accounts.repository.AccountsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class AccountsController {
 
     @Autowired
     private AccountsRepository accountsRepository;
+
+    @Autowired
+    AccountsServiceConfig accountsServiceConfig;
 
     @PostMapping("/myAccount")
     public Accounts getAccountDetails(@RequestBody Customer customer) {
@@ -22,5 +35,15 @@ public class AccountsController {
         } else {
             return null;
         }
+    }
+
+    @GetMapping("/account/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter objectWriter = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(accountsServiceConfig.getMsg(), accountsServiceConfig.getBuildVersion(),
+                accountsServiceConfig.getMailDetails(), accountsServiceConfig.getActiveBranches());
+
+        String jsonStr = objectWriter.writeValueAsString(properties);
+        return jsonStr;
     }
 }
